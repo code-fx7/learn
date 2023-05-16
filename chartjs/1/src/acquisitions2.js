366c60e2a4d6a336a86cd3eb6bd9e1c80512591f
Christoph Feliksiak <christoph.feliksiak@connedata.net>
2023-05-16 10:37:38 +0200

    add chartjs/1

---
+++ chartjs/1/src/acquisitions2.js
@@ -0,0 +1,42 @@
+import {
+    Chart,
+    Colors,
+    BarController,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Legend
+  } from 'chart.js'
+  
+  Chart.register(
+    Colors,
+    BarController,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    Legend
+  );
+  
+import { getAquisitionsByYear } from './api';
+
+import Chart from 'chart.js/auto'
+
+(async function() {
+    const data = await getAquisitionsByYear();
+
+  new Chart(
+    document.getElementById('acquisitions'),
+    {
+      type: 'bar',
+      data: {
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map(row => row.count)
+          }
+        ]
+      }
+    }
+  );
+})();
