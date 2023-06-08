76e03387555879671c10dc934697e733b4abc166
code-fx <code-fx@mail.de>
2023-06-08 16:37:43 +0200

    add reactiveForms

---
+++ angular/learn-angular/greenfield/reactiveForms/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  // FormGroup Schreibmöglichkeit (eher für einfache Formulare)
+  public loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5)
+    ], []),
+    password: new FormControl('', [
+      Validators.required
+    ], [])
+  });
+  constructor() {
+    this.loginForm.valueChanges.subscribe(console.log);
+  }
+}
+
+
+  // FormBuilder Schreibmöglichkeit (eher für komplexe/dynamische Formulare)
+
+//   public loginFormFB: FormGroup;
+
+//   constructor(private _fb: FormBuilder) {
+//     this.loginFormFB = this._fb.group({
+//       username: ['', [
+//         Validators.required,
+//         Validators.minLength(5)
+//       ], []],
+//       password: ['', [
+//         Validators.required
+//       ], []]
+//     });
+//   }
+// }
