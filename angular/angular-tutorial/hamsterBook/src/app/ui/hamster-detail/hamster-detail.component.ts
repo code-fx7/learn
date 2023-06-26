384839ed9875f752179b70ce62e31ddbd7d52fd7
code-fx <code-fx@mail.de>
2023-06-26 16:34:24 +0200

    add hamster-detail

---
+++ angular/angular-tutorial/hamsterBook/src/app/ui/hamster-detail/hamster-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marius',
+  templateUrl: './hamster-detail.component.html',
+  styleUrls: ['./hamster-detail.component.scss']
+})
+export class HamsterDetailComponent {
+
+}
