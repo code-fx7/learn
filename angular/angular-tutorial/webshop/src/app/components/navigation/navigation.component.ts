fa68b6f0fa5465734ae89d0cfa1e36271202df7d
code-fx <code-fx@mail.de>
2023-07-05 15:50:30 +0200

    edit navigation component

---
+++ angular/angular-tutorial/webshop/src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+
+export class NavigationComponent {
+  @Input() sidenav: MatSidenav;
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
+}
