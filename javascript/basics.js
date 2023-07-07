d021a5f6645845653932cda443c2804f53e06d41
code-fx <code-fx@mail.de>
2023-07-07 13:19:41 +0200

    add basics.js

---
+++ javascript/basics.js
@@ -0,0 +1,14 @@
+
+let num = 5;
+console.log(num++);
+console.log(num);
+console.log("------");
+
+let num1 = 5;
+console.log(++num1);
+console.log("------")
+
+console.log(~-5);
+console.log(-~5);
+console.log(~-"5");
+console.log(~-[][1]);
