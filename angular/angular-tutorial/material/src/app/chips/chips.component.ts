d6ca46b16fe5164978f55663659861f274689b51
code-fx <code-fx@mail.de>
2023-07-07 11:09:17 +0200

    add chips

---
+++ angular/angular-tutorial/material/src/app/chips/chips.component.ts
@@ -0,0 +1,51 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Component, inject } from '@angular/core';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { FormControl } from '@angular/forms';
+import { ThemePalette } from '@angular/material/core';
+
+export interface ChipColor {
+  name: string;
+  color: ThemePalette;
+}
+
+@Component({
+  selector: 'app-chips',
+  templateUrl: './chips.component.html',
+  styleUrls: ['./chips.component.scss']
+})
+export class ChipsComponent {
+  availableColors: ChipColor[] = [
+    {name: 'none', color: undefined},
+    {name: 'Primary', color: 'primary'},
+    {name: 'Accent', color: 'accent'},
+    {name: 'Warn', color: 'warn'},
+  ];
+  
+  keywords = ['angular', 'how-to', 'tutorial', 'accessibility'];
+  
+  formControl = new FormControl(['angular']);
+
+  announcer = inject(LiveAnnouncer);
+
+  removeKeyword(keyword: string) {
+    const index = this.keywords.indexOf(keyword);
+    if (index >= 0) {
+      this.keywords.splice(index, 1);
+
+      this.announcer.announce(`removed ${keyword}`);
+    }
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our keyword
+    if (value) {
+      this.keywords.push(value);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+}
