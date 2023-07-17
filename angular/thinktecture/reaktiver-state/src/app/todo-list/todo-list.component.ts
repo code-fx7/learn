636757a73ef90833fe0cf1b92010518c5d5ccf67
code-fx <code-fx@mail.de>
2023-07-17 12:08:51 +0200

    edit todo-list.component.ts

--- angular/thinktecture/reaktiver-state/src/app/todo-list/todo-list.component.ts
+++ angular/thinktecture/reaktiver-state/src/app/todo-list/todo-list.component.ts
@@ -2,12 +2,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
 import { TodoListItem } from '../todo.model';
+import { TodoListStore } from '../todo.list.store';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [TodoListStore],
 })
 export class TodoListComponent {
   readonly columns = ['done', 'content'];
@@ -18,7 +20,7 @@ export class TodoListComponent {
 
   readonly addDisabled$ = this.todoListStore.addDisabled$;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private todoListStore: TodoListStore) {}
 
   ngOnInit(): void {
     const id$ = this.route.paramMap.pipe(map(params => params.get('id') ?? ''));
@@ -34,5 +36,15 @@ export class TodoListComponent {
     this.todoListStore.updateItem({ id: item.id, update: { done } });
   }
 
-  setContent(item: TodoListItem, content: string): void
+  setContent(item: TodoListItem, content: string): void {
+    this.todoListStore.updateItem({ id: item.id, update: { content } });
+  }
+
+  edit(id?: string): void {
+    this.todoListStore.editItem(id);
+  }
+
+  cancelEdit(): void {
+    this.todoListStore.editItem(undefined);
+  }
 }
