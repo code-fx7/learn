64ac7a4083fb82f6153a3d72c4490ea1cf367c29
code-fx <code-fx@mail.de>
2023-07-17 12:11:29 +0200

    add todo.list.store.ts

---
+++ angular/thinktecture/reaktiver-state/src/app/todo.list.store.ts
@@ -0,0 +1,54 @@
+import { Injectable } from "@angular/core";
+import { ComponentStore, tapResponse } from '@ngrx/component-store';
+import { TodoListItem } from '../todo.model';
+import { Observable, switchMap } from "rxjs";
+import { TodoService } from "./todo.service";
+
+interface TodoListState {
+  id: string;
+  title: string;
+  items: TodoListItem[];
+  loading: boolean;
+
+  editing?: string;
+}
+
+@Injectable()
+export class TodoListStore extends ComponentStore<TodoListState> {
+  // Default Selectors
+  readonly title$ = this.select((state) => state.title);
+  readonly items$ = this.select(( { items }) => items);
+  readonly editing$ = this.select(( { editing }) => editing);
+  readonly loading$ = this.select(( { loading }) => loading):
+
+  // View Model
+  readonly addDisabled$ = this.select(this.loading$, this.editing$, (loading, editing) => {
+    return loading || editing !== undefined;
+  });
+
+  readonly editItem = this.updater((state, id: string | undefined) => {
+    return { ...state, editing: id };
+  });
+
+  readonly loadList = this.effect((id$: Observable<string>) =>
+  id$.pipe(
+    switchMap(id => {
+      this.patchState({ loading: true });
+      return this.todoService.getList(id).pipe(
+        tapResponse(
+          list=> {
+            this.patchState({ ...list, loading: false, editing: undefined });
+          },
+          () => {
+            this.patchState({ loading: false });
+          },
+        ),
+      );
+    }),
+  ),
+);
+  
+  constructor(private todoService: TodoService) {
+    super({ id: '', title: '', items: [], loading: true });
+  }
+}
