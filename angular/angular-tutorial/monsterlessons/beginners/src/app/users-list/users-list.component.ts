30a4c75cadc36f83c027e262cd4b2b3af94d315a
code-fx <code-fx@mail.de>
2023-07-17 09:55:26 +0200

    add monsterlessons

---
+++ angular/angular-tutorial/monsterlessons/beginners/src/app/users-list/users-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-list',
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.scss']
+})
+export class UsersListComponent {
+  testUser = 'Hi I am a test user'
+}
