638c14aec1b0a54f479b50e170107c5cd9ca2ee0
code-fx <code-fx@mail.de>
2023-07-26 09:12:37 +0200

    edit app.module.ts

--- angular/angular-tutorial/flight-booking/src/app/app.module.ts
+++ angular/angular-tutorial/flight-booking/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { AboutComponent } from './about/about.component';
+import { BasketComponent } from './basket/basket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    BasketComponent
   ],
   imports: [
     BrowserModule,
