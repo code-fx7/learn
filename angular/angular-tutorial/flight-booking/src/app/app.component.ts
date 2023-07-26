dbe060d363571c1ba91cea01e0ef709699a14d14
code-fx <code-fx@mail.de>
2023-07-26 16:51:43 +0200

    edit app.component

--- angular/angular-tutorial/flight-booking/src/app/app.component.ts
+++ angular/angular-tutorial/flight-booking/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router
+} from '@angular/router';
+import { OAuthServive } from '@angular-oauth2-oidc';
+import { authConfig } from './auth.config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'flight-booking';
+export class AppComponent implements OnInit {
+  title = 'Hello World!';
+  showWaitInfo = false;
+
+  constructor(
+    private oauthService: OAuthServive,
+    private router: Router) {
+      oauthService.configure(authConfig);
+      oauthService.loadDiscoveryDocumentAndTryLogin();
+    }
+
+    ngOnInit(): void {
+        this.router.events.subscribe(event => {
+          if (event instanceof NavigationStart) {
+            this.showWaitInfo = true;
+          }
+          else if (event instanceof NavigationEnd
+            || event instanceof NavigationCancel
+            || event instanceof NavigationError) {
+              this.showWaitInfo = false;
+            }
+        });
+    }
 }
