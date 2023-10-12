0a23d348131e1b2a5ef8a0c840129499007a2a31
code-fx <code-fx@mail.de>
2023-10-12 14:54:37 +0200

    add

---
+++ udemy/03-starting-with-routing/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: WelcomeComponent,
+  },
+  {
+    path: 'about',
+    // component: AboutComponent,
+    // Lazy Loading einer Standalone Component
+    loadComponent: () => 
+      import('./about/about.component').then((mod) => mod.AboutComponent),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/routes').then(
+        (mod) => mod.DASHBOARD_ROUTES
+      ),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
