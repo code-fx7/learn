78cd679b49c56d9a528e8a8c247c2bff3d83c76b
code-fx <code-fx@mail.de>
2023-10-17 16:02:32 +0200

    edit

--- angular/angular-tutorial/routing-app/src/app/app-routing.module.ts
+++ angular/angular-tutorial/routing-app/src/app/app-routing.module.ts
@@ -1,10 +1,56 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import { ResolveFn, RouteReuseStrategy, RouterModule, RouterStateSnapshot, Routes, TitleStrategy } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
-const routes: Routes = [];
+// const routes: Routes = [
+//   { path: 'first-component', component: FirstComponent },
+//   { path: 'second-component', component: SecondComponent },
+//   { path: '', redirectTo: './first-componentn', pathMatch: 'full' },
+//   { path: '**', component: FirstComponent },
+// ];
+
+const routes: Routes = [
+  {
+    path: 'first-component', 
+    title: 'First component',
+    component: FirstComponent, // Dies ist die Komponente mit dem <router-outlet> in der Vorlage
+    children: [
+      {
+        path: 'child-a', // untergeordneter Routenpfad
+        title: resolvedChildATitle,
+        component: ChildAComponent, // untergeordnete Routenkomponente, die der Router rendert
+      },
+      {
+        path: 'child-b',
+        title: 'child-b',
+        component: ChildBComponent, // Eine weitere untergeordnete Routenkomponente, die der Router rendert
+      },
+    ],
+  },
+];
+
+const resolvedChildATitle: ResolveFn<string> = () => Promise.resolve('child a');
+
+@Injectable({providedIn: 'root'})
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState):
+    if (title !== undefined) {
+      this.title.setTitle(`My Application | ${title}`)
+    }
+  }
+}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {provide: TitleStrategy, useClass: TemplatePageTitleStrategy},
+  ]
 })
 export class AppRoutingModule { }
